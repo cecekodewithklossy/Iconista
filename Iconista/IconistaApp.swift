6747239c09a7da21083d0775e711f5c9f38f2c37
Cecilia Fierro <cecilia.fierro@icloud.com>
2023-06-12 15:37:35 -0400

    Initial Commit

---
+++ Iconista/IconistaApp.swift
@@ -0,0 +1,17 @@
+//
+//  IconistaApp.swift
+//  Iconista
+//
+//  Created by Cecilia Fierro on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct IconistaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
